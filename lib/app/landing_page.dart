4209338d86df7536c674741b14f013cde1980abd
ayogeshwaran <ayogeshwaran@vmware.com>
2019-07-23 23:42:28 +0530

    Removing state management related code and making use of StreamBuilder event to switch pages

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,64 +1,30 @@
-import 'dart:async';
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/home_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
-class LandingPage extends StatefulWidget {
+class LandingPage extends StatelessWidget {
   LandingPage({@required this.auth});
   final AuthBase auth;
 
   @override
-  _LandingPageState createState() => _LandingPageState();
-}
-
-class _LandingPageState extends State<LandingPage> {
-  User _user;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkCurrentUser();
-    widget.auth.onAuthStateChanged.listen((user) {
-      print('User id: ${user?.uid}');
-    });
-  }
-
-  Future<void> _checkCurrentUser() async {
-     User user = await widget.auth.currentUser();
-    _updateUser(user);
-  }
-
-  void _updateUser(User user) {
-    setState(() {
-      _user = user;
-    });
-  }
-
-  void _signOut() {
-    setState(() {
-      _user = null;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: widget.auth.onAuthStateChanged,
+    return StreamBuilder<User>(
+      stream: auth.onAuthStateChanged,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           User user = snapshot.data;
           if (user == null) {
             return SignInPage(
-              onSignIn: _updateUser,
-              auth: widget.auth,
+              auth: auth,
             );
           }
           return HomePage(
-            onSignOut: _signOut,
-            auth: widget.auth,
+            auth: auth,
+          );
+        } else {
+          return Scaffold(
+            body: Center(child: CircularProgressIndicator()),
           );
         }
       });
