052abb9888caee0692f3fffea5575e91bfc8aecf
ayogeshwaran <ayogeshwaran@vmware.com>
2019-07-23 19:18:04 +0530

    Using Dependency Injection to pass Auth instance to the classes

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,18 +1,19 @@
 import 'dart:async';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/social_sign_in_button.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class SignInPage extends StatelessWidget {
-  SignInPage({@required this.onSignIn});
+  SignInPage({@required this.onSignIn, @required this.auth});
 
-  final Function(FirebaseUser) onSignIn;
+  final Function(User) onSignIn;
+  final AuthBase auth;
 
   Future<void> _signInAnonymously() async {
     try {
-      FirebaseUser user = await FirebaseAuth.instance.signInAnonymously();
+      User user = await auth.signInAnonymously();
       print('User id: ${user.uid}');
       onSignIn(user);
     } catch (e) {
