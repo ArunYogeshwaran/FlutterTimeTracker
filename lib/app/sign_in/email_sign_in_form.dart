356b654e5d8747a52026a688c0227c86e43f6cdc
ayogeshwaran <ayogeshwaran@vmware.com>
2019-07-29 19:07:15 +0530

    Creating a new page to support email and password signin

---
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/validators.dart';
+import 'package:time_tracker_flutter_course/common_widgets/form_submit_button.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+enum EmailSignInFormType { signIn, register }
+
+class EmailSignInForm extends StatefulWidget with EmailAndPasswordValidators {
+  EmailSignInForm({@required this.authBase});
+
+  final AuthBase authBase;
+
+  @override
+  _EmailSignInFormState createState() => _EmailSignInFormState();
+}
+
+class _EmailSignInFormState extends State<EmailSignInForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+
+  String get _email => _emailController.text;
+  String get _password => _passwordController.text;
+  EmailSignInFormType _formType = EmailSignInFormType.signIn;
+  bool _submitted = false;
+
+  void _submit() async {
+    setState(() {
+      _submitted = true;
+    });
+    try {
+      if (_formType == EmailSignInFormType.signIn) {
+        await widget.authBase.signInWithEmailAndPassword(_email, _password);
+      } else {
+        await widget.authBase.createUserWithEmailAndPassword(_email, _password);
+      }
+      Navigator.of(context).pop();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void _toggleFormType() {
+    setState(() {
+      _submitted = false;
+      _formType = _formType == EmailSignInFormType.signIn
+          ? EmailSignInFormType.register
+          : EmailSignInFormType.signIn;
+    });
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
+  List<Widget> _buildChildren() {
+    final primaryText = _formType == EmailSignInFormType.signIn
+        ? "Sign in"
+        : 'Create an account';
+    final secondaryText = _formType == EmailSignInFormType.register
+        ? 'Need an account? Register'
+        : 'Have an account? Sign in';
+    bool submitEnabled = widget.emailValidator.isValid(_email) &&
+        widget.passwordValidator.isValid(_password);
+
+    return [
+      _buildEmailTextField(),
+      SizedBox(
+        height: 8.0,
+      ),
+      _buildPasswordTextField(),
+      SizedBox(
+        height: 8.0,
+      ),
+      FormSubmitButton(
+        text: primaryText,
+        onPressed: submitEnabled ? _submit :  null,
+      ),
+      SizedBox(
+        height: 8.0,
+      ),
+      FlatButton(
+        child: Text(secondaryText),
+        onPressed: _toggleFormType,
+      ),
+    ];
+  }
+
+  TextField _buildEmailTextField() {
+    bool showErrorText = _submitted && !widget.emailValidator.isValid(_email);
+    return TextField(
+      controller: _emailController,
+      focusNode: _emailFocusNode,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: 'test@test.com',
+        errorText: showErrorText ? widget.emptyEmail: null,
+      ),
+      autocorrect: false,
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
+      onChanged: (email) => _updateState(),
+      onEditingComplete: _emailEditingComplete,
+    );
+  }
+
+  TextField _buildPasswordTextField() {
+    bool showErrorText = _submitted && !widget.passwordValidator.isValid(_password);
+    return TextField(
+      controller: _passwordController,
+      focusNode: _passwordFocusNode,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        errorText: showErrorText ? widget.emptyPassword : null,
+      ),
+      obscureText: true,
+      textInputAction: TextInputAction.done,
+      onChanged: (password) => _updateState(),
+      onEditingComplete: _submit,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
+    );
+  }
+
+  void _emailEditingComplete() {
+    FocusScope.of(context).requestFocus(_passwordFocusNode);
+  }
+
+  _updateState() {
+    setState(() {});
+  }
+}
---
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_form.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class EmailSignInPage extends StatelessWidget {
+  EmailSignInPage({@required this.authBase});
+  final AuthBase authBase;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Sign in')),
+        elevation: 4.0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          child: EmailSignInForm(authBase: authBase,),
+        ),
+      ),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+}
+
