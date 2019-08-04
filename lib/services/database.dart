76649478c0d661ca8c35f03f69758d9ef1161618
ayogeshwaran <ayogeshwaran@vmware.com>
2019-08-05 00:02:46 +0530

    Adding delete support with firebase

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -5,10 +5,12 @@ import 'package:time_tracker_flutter_course/app/home/models/job.dart';
 import 'package:time_tracker_flutter_course/services/api_path.dart';
 import 'package:time_tracker_flutter_course/services/firestore_service.dart';
 
-
 abstract class Database {
   Future<void> setJob(Job job);
+
   Stream<List<Job>> jobsStream();
+
+  Future<void> deleteJob(Job job);
 }
 
 String documentIdFromCurrentDate() => DateTime.now().toIso8601String();
@@ -19,11 +21,17 @@ class FirestoreDatabase implements Database {
 
   final _service = FirestoreService.instance;
 
+  @override
   Future<void> setJob(Job job) async => await _service.setData(
         path: APIPath.job(uid, job.id),
         data: job.toMap(),
       );
 
+  @override
+  Future<void> deleteJob(Job job) async =>
+      await _service.deleteData(path: APIPath.job(uid, job.id),);
+
+  @override
   Stream<List<Job>> jobsStream() {
     return _service.collectionStream(
       path: APIPath.jobs(uid),
