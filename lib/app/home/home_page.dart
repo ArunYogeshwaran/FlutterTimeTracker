8d1acfbd20c4337627db600dcad869d520c9fc64
ayogeshwaran <ayogeshwaran@vmware.com>
2019-08-17 12:56:42 +0530

    Handling selection of bottom navigation bar items

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/home/cupertino_home_scaffold.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/jobs_page.dart';
 import 'package:time_tracker_flutter_course/app/home/tab_item.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,11 +11,20 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TabItem _currentTab = TabItem.jobs;
 
+  Map<TabItem, WidgetBuilder> get widgetBuilders {
+    return {
+      TabItem.jobs: (_) => JobsPage(),
+      TabItem.entires: (_) => Container(),
+      TabItem.account: (_) => Container(),
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoHomeScaffold(
       currentTab: _currentTab,
       onSelectTab: _select,
+      widgetBuilders: widgetBuilders,
     );
   }
 
